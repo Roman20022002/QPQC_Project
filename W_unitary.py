c8a165bc9769714966816da97e5d572b313a0cf9
aglas2001 <65231889+aglas2001@users.noreply.github.com>
2024-01-19 11:21:10 +0100

    .

--- W_unitary.py
+++ W_unitary.py
@@ -8,34 +8,31 @@ def theta_gate(qc,theta,i):
     qc.rz(theta[2],i)
     return 0
 
-def add_Z_gates(qc, n, j):
+def add_Z_gates(qc, n, j,complicated_entangle):
     if n >2:
-        connects = []
-        for k in range(j%(n-1)+1):
-            connects+=range(k,n,j%(n-1)+1)
-        for i in range(len(connects)-1):
-            qc.cz(connects[i],connects[i+1])
-        qc.cz(connects[-1],connects[0])
-    if n ==2:
+        if complicated_entangle:
+            connects = []
+            for k in range(j%(n-1)+1):
+                connects+=range(k,n,j%(n-1)+1)
+            for i in range(len(connects)-1):
+                qc.cz(connects[i],connects[i+1])
+            qc.cz(connects[-1],connects[0])
+        else:
+            if j%2==0:
+                for i in range(n-1):
+                    qc.cz(i,i+1)
+            else:
+                for i in range(n-1,0,-1):
+                    qc.cz(i-1,i)    
+    elif n ==2:
         qc.cz(0,1)
     return 0
 
 
-def W(qc,theta, n, l):
+def W(qc,theta, n, l, complicated_entangle =False):
     Theta_matrix = np.reshape(theta, (n,l,3))
     for j in range(l):
         for i in range(n):
             theta_gate(qc,Theta_matrix[i,j,:],i)
-        add_Z_gates(qc, n, j)
+        add_Z_gates(qc, n, j, complicated_entangle)
     return qc
-
-# ## Initialize
-# n = 4
-# d = 4
-
-# ## Initial Theta, this is a vector of 3*n*d elements, here implemented as nxdx3 array.
-# ## Theta is obtained frrom training with some minimisation method.
-# Theta = np.round(np.random.rand(n,d,3),2)
-
-# qc = W(Theta, n, d)
-# qc.draw()

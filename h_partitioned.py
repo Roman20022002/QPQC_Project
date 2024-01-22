2146878f00e43e1f74a56380fe677ecaed71ce50
peterschaefers <p.schaefers@student.tudelft.nl>
2024-01-22 13:55:29 +0100

    h_partitioned

--- h_partitioned.py
+++ h_partitioned.py
@@ -30,6 +30,13 @@ def expectation(circuit,shots):
 def phi_ij(xi,xj):
     return (np.pi-xi)*(np.pi-xj)
 
+def theta_gate(qc,theta,i):
+    assert len(theta) == 3, "theta incorrect size"
+    qc.rx(theta[0],i)
+    qc.ry(theta[1],i)
+    qc.rz(theta[2],i)
+    return 0
+
 def U1(x,n_part,circuit):
     circuit.h(range(n_part))
 
@@ -159,19 +166,44 @@ def V4(x,n_part,circuit):
     circuit.cx(0,1) 
     return circuit
 
+def W1(qc,theta, n, n_part, l=1):
+    Theta_matrix = np.reshape(theta, (n,l,3))[0:2,:,:]
+    for j in range(l):
+        for i in range(n_part):
+            theta_gate(qc,Theta_matrix[i,j,:],i)
+    qc.cz(0,1)        
+    return qc
+
+def W2(qc,theta, n, n_part, l=1):
+    Theta_matrix = np.reshape(theta, (n,l,3))[2:4,:,:]
+    for j in range(l):
+        for i in range(n_part):
+            theta_gate(qc,Theta_matrix[i,j,:],i)
+    return qc
+
+def M1(qc):
+    return qc
+
+def M2(qc):
+    qc.cz(0,1)   
+    return qc
+
 U_partitioned = [U1,U2,U3,U4]
 V_partitioned = [V1,V2,V3,V4]
+W_partitioned = [W1,W2]
+M_partitioned = [M1,M2]
 
-def h_partitioned(x,n_part,d,shots):
+def h_partitioned(x,n,n_part,d,shots,theta):
     #Initialize circuit
     circuit1 = qk.QuantumCircuit(n_part,n_part)
     circuit2 = qk.QuantumCircuit(n_part,n_part)
+    circuit3 = qk.QuantumCircuit(n_part,n_part)
 
     assert d<=2, "d must be smaller or equal to 2"
 
     expectation_value = 0
     if d == 1:
-        combinations = [(i, i) for i in range(0,4)]
+        combinations = [(i, i, j) for i in range(0,4) for j in range(0,2)]
         for i in range(len(combinations)):
             circuit1 = U_partitioned[combinations[i][0]](x,n_part,circuit1)
             circuit1.barrier()
@@ -183,22 +215,33 @@ def h_partitioned(x,n_part,d,shots):
             circuit2.z(range(n_part))
             circuit2.measure(range(n_part),range(n_part))
 
+            circuit3 = W_partitioned[combinations[i][2]](circuit3,theta,n,n_part)
+            circuit3.barrier()
+            circuit3.z(range(n_part))
+            circuit3.measure(range(n_part),range(n_part))
+
             expectation_1 = expectation(circuit1,shots)
             expectation_2 = expectation(circuit2,shots)
 
             expectation_value += expectation_1*expectation_2
 
     if d == 2:
-        combinations = [((i, i), (j, j)) for i in range(0, 4) for j in range(0, 4)]
+        combinations = [((i, i, k), (j, j, l)) for i in range(0, 4) for j in range(0, 4) for k in range(0,2) for l in range(0,2)]
         for i in range(len(combinations)):
             circuit1 = U_partitioned[combinations[i][0][0]](x,n_part,circuit1)
+            circuit1.barrier()
             circuit1 = V_partitioned[combinations[i][0][1]](x,n_part,circuit1)
+            circuit1.barrier()
+            circuit1 = W_partitioned[combinations[i][0][2]](circuit1,theta,n,n_part)
             circuit1.barrier()
             circuit1.z(range(n_part))
             circuit1.measure(range(n_part),range(n_part))
 
             circuit2 = U_partitioned[combinations[i][1][0]](x,n_part,circuit2)
+            circuit2.barrier()
             circuit2 = V_partitioned[combinations[i][1][1]](x,n_part,circuit2)
+            circuit2.barrier()
+            circuit2 = M_partitioned[combinations[i][1][2]](circuit2,theta,n,n_part)
             circuit2.barrier()
             circuit2.z(range(n_part))
             circuit2.measure(range(n_part),range(n_part))
